aa05b913b63c4e1e25333ab91763a525e99ab9fd
DEVMGO <devgolmohammadi@gmail.com>
2021-10-26 10:33:34 +0330

    make component Cover and add to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import Navbar from "./components/navbar/Navbar";
+import Cover from "./components/cover/Cover";
+import './assets/styles/app.css';
 
 function App() {
   return (
     <div className="app">
-      <Navbar/>
+      <div className="app-navbar">
+        <Navbar />
+      </div>
+      <div className="app-cover">
+        <Cover />
+      </div>
     </div>
   );
 }
---
+++ src/components/cover/Cover.jsx
@@ -0,0 +1,26 @@
+import bgvideo from '../../assets/videos/bgvideo.mp4';
+import imgcover from '../../assets/img/back.jpg';
+import '../../assets/styles/cover.css';
+
+const Cover = () => {
+    return (
+        <section id="cover" >
+
+            <div className="cover-video">
+                <video src={`${bgvideo}`} id="herovideo"
+                    playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop" width="100%" height="100%">
+                    <source type="video/mp4" src={`${bgvideo}`}></source>
+                </video>
+                <img src={`${imgcover}`} alt="cover-image" />
+            </div>
+
+            <div className="cover-text col-md-12">
+                <p>DornaWing - We brings applied drone to people life!</p>
+                <p>Check out our News or LinkedIn page to follow the activities!</p>
+            </div>
+            
+        </section>
+    );
+}
+
+export default Cover;
