6b1bbb18758982b505e3d86891920c65fb386acb
DEVMGO <devgolmohammadi@gmail.com>
2021-10-26 17:43:28 +0330

    edit Navbar (scroll , logo , tabs , responsive)

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,38 +1,75 @@
+import * as React from 'react';
+import { useEffect, useState, useRef } from 'react';
+import logodornawing from '../../assets/img/logo.png';
+import { BsPersonFill } from 'react-icons/bs';
+import { IoCart } from "react-icons/io5";
+import '../../assets/styles/navbar.css';
+
 const Navbar = () => {
+
+    // change Size Navbar with scroll
+    const [navSize, setNavSize] = useState(false)
+    const sizeRef = React.useRef()
+    sizeRef.current = navSize
+    useEffect(() => {
+        const handleScroll = () => {
+            const show = window.scrollY > 270
+            if (sizeRef.current !== show) {
+                setNavSize(show)
+            }
+        }
+        document.addEventListener('scroll', handleScroll)
+        return () => {
+            document.removeEventListener('scroll', handleScroll)
+        }
+    }, [])
+
+
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+        <nav style={{
+            background: navSize ? '#0b2534' : 'rgba(0, 0, 0, 0.3)',
+            height: navSize ? '60px' : '80px', transition: '1s ease',
+        }} className="navbar navbar-light navbar-expand-lg fixed-top col-sm-12">
+
+            <div className="container nav-cont">
+                <a className="navbar-brand" href="#" style={{ width: navSize ? '4%' : '5%', padding: '0', transition: '1s'}}>
+                    <img src={logodornawing} alt="dornawing" style={{ width: '100%', height: '100%', backgroundcolor: 'secondary' }} />
+                </a>
+                <button className="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active"href="#" style={{marginBottom: navSize ? '-5px' : '',}}>Products</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#" style={{marginBottom: navSize ? '-5px' : '',}}>NewsRoom</a>
                         </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li><hr class="dropdown-divider"/></li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#" style={{marginBottom: navSize ? '-5px' : '',}} >Contact</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#" style={{marginBottom: navSize ? '-5px' : '',}} >About Us</a>
                         </li>
                     </ul>
-                    <form class="d-flex">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                        <button class ="btn btn-outline-success" type ="submit">Search</button>
-                    </form>
+                    <div className="d-flex form-nav">
+                        <a className="shop-btn" href=""><IoCart /></a>
+
+                        <div className="btn btn-secondary dropdown user-btn" type="submit">
+                            <a id="navbarDropdown" data-bs-toggle="dropdown">
+                                <BsPersonFill />
+                            </a>
+                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a className="dropdown-item" href="#">Profile</a></li>
+                                <li><a className="dropdown-item" href="#">Other Acant</a></li>
+                                <li><hr className="dropdown-divider" /></li>
+                                <li><a className="dropdown-item" href="#">Log Out</a></li>
+                            </ul>
+
+                        </div>
+                    </div>
                 </div>
             </div>
         </nav>
