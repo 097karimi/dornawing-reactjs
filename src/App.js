75203aa8986026832ebbc61e9ebe1e54d8e5a8b6
DEVMGO <devgolmohammadi@gmail.com>
2021-10-30 11:21:35 +0330

    Fixed some bugs and added component (Open_Close_Nav.jsx) for open and close the menu responsibly

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import Navbar from "./components/navbar/Navbar";
 import Cover from "./components/cover/Cover";
 import './assets/styles/app.css';
+import Test from "./components/test/Test";
 
 function App() {
+  
   return (
     <div className="app">
-      <div className="app-navbar">
-        <Navbar />
-      </div>
-      <div className="app-cover">
-        <Cover />
+      <div className="container-fuled">
+        <div className="row ">
+          <div className="app-navbar">
+            <Navbar />
+          </div>
+          <div className="app-cover col-md-12">
+            <Cover />
+          </div>
+          <div className="app-test col-md-12">
+            <Test />
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,10 +4,12 @@ import logodornawing from '../../assets/img/logo.png';
 import { BsPersonFill } from 'react-icons/bs';
 import { IoCart } from "react-icons/io5";
 import '../../assets/styles/navbar.css';
+import { openNav } from './Open_Close_Nav';
+import { closeNav } from './Open_Close_Nav';
 
 const Navbar = () => {
 
-    // change Size Navbar with scroll
+    // change Color & Size Navbar with scroll
     const [navSize, setNavSize] = useState(false)
     const sizeRef = React.useRef()
     sizeRef.current = navSize
@@ -24,52 +26,52 @@ const Navbar = () => {
         }
     }, [])
 
-
     return (
-        <nav style={{
-            background: navSize ? '#0b2534' : 'rgba(0, 0, 0, 0.3)',
-            height: navSize ? '60px' : '80px', transition: '1s ease',
-        }} className="navbar navbar-light navbar-expand-lg fixed-top col-sm-12">
+        <nav style={{ background: navSize ? '#0b2534' : 'rgba(0, 0, 0, 0.3)', height: navSize ? '60px' : '80px', transition: '1s' }}
+            className="navbar navbar-expand-lg navbar-light fixed-top" id="nav">
 
-            <div className="container nav-cont">
-                <a className="navbar-brand" href="#" style={{ width: navSize ? '4%' : '5%', padding: '0', transition: '1s'}}>
-                    <img src={logodornawing} alt="dornawing" style={{ width: '100%', height: '100%', backgroundcolor: 'secondary' }} />
+            <div className="container-fluid" id="confluid">
+                <a className="navbar-brand" href="#">
+                    <img id="navbrand" src={logodornawing} alt="dornawing" style={{ width: navSize ? '80%' : '90%' }} />
                 </a>
-                <button className="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                <button className="navbar-toggler" type="button" id="button-toggler">
+                    <span id="open" onClick={openNav} >&#9776;</span>
                 </button>
+
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <span id="close" className="open" onClick={closeNav} >&times;</span>
+
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0" style={{ fontSize: navSize ? '.9rem' : '1.1rem', }}>
                         <li className="nav-item">
-                            <a className="nav-link active"href="#" style={{marginBottom: navSize ? '-5px' : '',}}>Products</a>
+                            <a className="nav-link active" href="#">Products</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#" style={{marginBottom: navSize ? '-5px' : '',}}>NewsRoom</a>
+                            <a className="nav-link" href="#">NewsRoom</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#" style={{marginBottom: navSize ? '-5px' : '',}} >Contact</a>
+                            <a className="nav-link" href="#" >Contact</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#" style={{marginBottom: navSize ? '-5px' : '',}} >About Us</a>
+                            <a className="nav-link" href="#" >About Us</a>
                         </li>
                     </ul>
-                    <div className="d-flex form-nav">
-                        <a className="shop-btn" href=""><IoCart /></a>
-
+                    <div className="d-flex form-nav" id="form-nav1">
+                        <a className="shop-btn" href="" style={{ fontSize: navSize ? '1.6rem' : '2rem' }}><IoCart /></a>
                         <div className="btn btn-secondary dropdown user-btn" type="submit">
-                            <a id="navbarDropdown" data-bs-toggle="dropdown">
-                                <BsPersonFill />
-                            </a>
-                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a className="dropdown-item" href="#">Profile</a></li>
-                                <li><a className="dropdown-item" href="#">Other Acant</a></li>
-                                <li><hr className="dropdown-divider" /></li>
-                                <li><a className="dropdown-item" href="#">Log Out</a></li>
+                            <a id="navbarDropdown" data-bs-toggle="dropdown" style={{
+                                width: navSize ? '1.3rem' : '2rem',
+                                right: navSize ? '.5rem' : '0.16rem'
+                            }}><BsPersonFill /></a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">Profile</a></li>
+                                <li><a class="dropdown-item" href="#">Other Acant</a></li>
+                                <li><hr class="dropdown-divider" /></li>
+                                <li><a class="dropdown-item" href="#">Log Out</a></li>
                             </ul>
 
                         </div>
                     </div>
+
                 </div>
             </div>
         </nav>
