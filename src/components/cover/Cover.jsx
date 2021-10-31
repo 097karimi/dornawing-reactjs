424e77d70008cf830ee67b26914be180d168380d
DEVMGO <devgolmohammadi@gmail.com>
2021-10-31 16:32:07 +0330

    refactor Cover

--- src/components/cover/Cover.jsx
+++ src/components/cover/Cover.jsx
@@ -2,14 +2,14 @@ import bgvideo from '../../assets/videos/bgvideo.mp4';
 import imgcover from '../../assets/img/back.jpg';
 import '../../assets/styles/cover.css';
 
-
 const Cover = () => {
     return (
         <section id="cover" >
 
             <div className="cover-video">
                 <video src={`${bgvideo}`} id="herovideo"
-                    playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop" width="100%" height="100%">
+                    playsinline="playsinline" autoplay="autoplay"
+                    muted="muted" loop="loop" width="100%" height="100%">
                     <source type="video/mp4" src={`${bgvideo}`}></source>
                 </video>
                 <img src={`${imgcover}`} alt="cover-image" />
@@ -19,7 +19,7 @@ const Cover = () => {
                 <p>DornaWing - We brings applied drone to people life!</p>
                 <p>Check out our News or LinkedIn page to follow the activities!</p>
             </div>
-            
+
         </section>
     );
 }
